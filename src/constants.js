752ebf5c70996593e196095257e76a9578137e6d
Siva Sandhya <kambhamsandhya983@gmail.com>
2026-09-14 21:43:56 +0530

    Add password generator constants

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const CHARACTER_SETS = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+
+  numbers: "0123456789",
+
+  symbols: "!@#$%^&*()_+-=[]{}|;:,.<>?",
+};
+
+export const PASSWORD_CONFIG = {
+  minLength: 4,
+  maxLength: 32,
+  defaultLength: 16,
+};
